c2a9723e3b35e841015b3c3e666c03eb1ccc504b
Jeremy Opriasa <opriasajbusiness@gmail.com>
2023-08-24 22:33:25 +0800

    feat: String Operations (Python Data Structures - Simple Exercise 2)

---
+++ StringOperations.py
@@ -0,0 +1,7 @@
+
+sample_input = input("Enter something: ")
+increment_multiplication = 1
+
+for i in sample_input:
+  print(i * increment_multiplication)
+  increment_multiplication = increment_multiplication + 1
